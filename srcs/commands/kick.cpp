bde54589f378e72840fb62e7eab9a0d9f2e26551
cde-sous <cde-sous@student.42.fr>
2025-05-12 13:18:48 +0200

    feature: error messages sent from server to client in kick.cpp, fix: message sent when kicking someone from the channel

--- srcs/commands/kick.cpp
+++ srcs/commands/kick.cpp
@@ -3,43 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   kick.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:58:18 by jewu              #+#    #+#             */
-/*   Updated: 2025/05/09 15:03:04 by codespace        ###   ########.fr       */
+/*   Updated: 2025/05/12 13:17:39 by cde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Server.hpp"
 #include "Client.hpp"
-
-// KICK #test codespac0 :message de la mort
+#include "Server.hpp"
 
 void kick(Client& client, Server& theServer, std::istringstream& iss)
 {
-	std::string channelName, clientToKick;
-	iss >> channelName >> clientToKick;
-	channelName = channelName.substr(1);
-	std::cout << "channel: " << channelName << " client to kick: " << clientToKick << std::endl;
-	// check if client is operator
-	ChannelIterator channel = theServer.findChannel(channelName);
-	std::cout << "SEARCHING" << std::endl;
-	if (channel != theServer.getChannels().end())
-	{
-		// check if clientB is in channel
-		std::cout << "FOUND CHANNEL" << std::endl;
-		removeClientFromChannel(clientToKick, *(*channel));
-		removeOperator(clientToKick, *(*channel));
-		if ((*channel)->getClients().size() == 0)
-		{
-			delete *channel;
-			channel = theServer.getChannels().erase(channel);
-		}
-		else if ((*channel)->getOperators().size() == 0)
-			(*channel)->giveOperatorRights((*channel)->getClients().begin());
-		std::string message;
-		std::getline(iss, message);
-		sendServerReply(client, KICK(client.getNickname(), channelName, clientToKick, message));
-		std::cout << "KICKED" << std::endl;
-	}
+    std::string channelName, clientToKick;
+    iss >> channelName >> clientToKick;
+    channelName             = channelName.substr(1);
+    ChannelIterator channel = theServer.findChannel(channelName);
+    if (channel != theServer.getChannels().end())
+    {
+        ClientIterator chanop = (*channel)->findOperator(client.getNickname());
+        if (chanop == (*channel)->getOperators().end())
+        {
+            sendServerReply(client,
+                            ERR_CHANOPRIVSNEEDED(client.getNickname(), channelName,
+                                                 client.getNickname() +
+                                                     " does not have the necessary privileges"));
+            return;
+        }
+        ClientIterator hasClient = (*channel)->findClient(clientToKick);
+        if (hasClient == (*channel)->getClients().end())
+        {
+            sendServerReply(client,
+                            ERR_USERNOTINCHANNEL(client.getNickname(), clientToKick, channelName,
+                                                 clientToKick + " is not in the channel"));
+            return;
+        }
+        removeClientFromChannel(clientToKick, *(*channel));
+        removeOperator(clientToKick, *(*channel));
+        if ((*channel)->getClients().size() == 0)
+        {
+            delete *channel;
+            channel = theServer.getChannels().erase(channel);
+        }
+        else if ((*channel)->getOperators().size() == 0)
+            (*channel)->giveOperatorRights((*channel)->getClients().begin());
+        std::string message;
+        std::getline(iss, message);
+        message = message.substr(2);
+        sendServerReply(client, KICK(client.getNickname(), channelName, clientToKick, message));
+    }
 }
