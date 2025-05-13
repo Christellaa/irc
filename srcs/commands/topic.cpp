21545233c16e096c018b55e779640dff0740571a
cde-sous <cde-sous@student.42.fr>
2025-05-13 14:38:07 +0200

    fix: don't show topic when no topic message

--- srcs/commands/topic.cpp
+++ srcs/commands/topic.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   topic.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jewu <jewu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 11:28:05 by jewu              #+#    #+#             */
-/*   Updated: 2025/05/13 13:41:33 by jewu             ###   ########.fr       */
+/*   Updated: 2025/05/13 14:37:00 by cde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,59 @@
 
 static bool client_is_in_channel(Client& client, Channel& channel)
 {
-	ClientVec clientsToSearch = channel.getClients();
-	for (ClientVec::iterator it = clientsToSearch.begin(); it != clientsToSearch.end(); ++it)
-	{
-		if (*it == &client)
-			return true;
-	}
-	return false;
+    ClientVec clientsToSearch = channel.getClients();
+    for (ClientVec::iterator it = clientsToSearch.begin(); it != clientsToSearch.end(); ++it)
+    {
+        if (*it == &client)
+            return true;
+    }
+    return false;
 }
 
 void topic(Client& client, Server& theServer, std::istringstream& iss)
 {
-	std::string channelName;
-	iss >> channelName;
-	if (!channelName.empty())
-		channelName = channelName.substr(1);
-	else
-		channelName = "";
-	ChannelIterator channel = theServer.findChannel(channelName);
-	if (channel == theServer.getChannels().end())
-	{
-		sendServerReply(client, ERR_NOSUCHCHANNEL(client.getNickname(), channelName, " :Channel not found"));
-		return ;
-	}
-	if (!client_is_in_channel(client, *(*channel)))
-	{
-		sendServerReply(client, ERR_NOTONCHANNEL(client.getNickname(), (*channel)->getName(), " :Client not in the channel"));
-		return ;
-	}
-	std::string message;
-	std::getline(iss, message);
-	message = message.substr(2);
-	if (message.empty())
-	{
-		if ((*channel)->getTopicMessage().empty())
-			sendServerReply(client, RPL_NOTOPICSET(client.getNickname(), (*channel)->getName(), " :No topic set"));
-		else
-			sendServerReply(client, RPL_TOPIC(client.getNickname(), (*channel)->getName(), " :" + (*channel)->getTopicMessage()));
-		return ;
-	}
-	ClientIterator chanop = (*channel)->findOperator(client.getNickname());
-    if ((*channel)->getTopicScope() && chanop == (*channel)->getOperators().end())
+    std::string channelName;
+    iss >> channelName;
+    if (!channelName.empty())
+        channelName = channelName.substr(1);
+    ChannelIterator channel = theServer.findChannel(channelName);
+    if (channel == theServer.getChannels().end())
     {
-        sendServerReply(client, ERR_CHANOPRIVSNEEDED(client.getNickname(), channelName, client.getNickname() + " does not have the necessary privileges"));
+        sendServerReply(
+            client, ERR_NOSUCHCHANNEL(client.getNickname(), channelName, " :Channel not found"));
         return;
     }
-	(*channel)->setTopicMessage(message);
-	ClientIterator it  = (*channel)->getClients().begin();
+    if (!client_is_in_channel(client, *(*channel)))
+    {
+        sendServerReply(client, ERR_NOTONCHANNEL(client.getNickname(), (*channel)->getName(),
+                                                 " :Client not in the channel"));
+        return;
+    }
+    std::string message;
+    std::getline(iss, message);
+    message = message.substr(2);
+    if (message.empty())
+    {
+        if ((*channel)->getTopicMessage().empty())
+            sendServerReply(client, RPL_NOTOPICSET(client.getNickname(), (*channel)->getName(),
+                                                   " :No topic set"));
+        else
+            sendServerReply(client, RPL_TOPIC(client.getNickname(), (*channel)->getName(),
+                                              " :" + (*channel)->getTopicMessage()));
+        return;
+    }
+    ClientIterator chanop = (*channel)->findOperator(client.getNickname());
+    if ((*channel)->getTopicScope() && chanop == (*channel)->getOperators().end())
+    {
+        sendServerReply(
+            client,
+            ERR_CHANOPRIVSNEEDED(client.getNickname(), channelName,
+                                 client.getNickname() + " does not have the necessary privileges"));
+        return;
+    }
+    (*channel)->setTopicMessage(message);
+    ClientIterator it  = (*channel)->getClients().begin();
     ClientIterator ite = (*channel)->getClients().end();
-	for (; it != ite; ++it)
-    	sendServerReply(*(*it), TOPIC(client.getNickname(), channelName, message));
+    for (; it != ite; ++it)
+        sendServerReply(*(*it), TOPIC(client.getNickname(), channelName, message));
 }
